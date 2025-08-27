c7e0a4dd8460fb078bec0094e12c7d342d8e08c5
strahd <jv.palha@gmail.com>
2025-08-27 20:27:49 -0300

    feat: listing all projects where user are a member of

--- lib/view_office/projects/services/all.ex
+++ lib/view_office/projects/services/all.ex
@@ -1,11 +1,26 @@
 defmodule ViewOffice.Projects.Services.All do
-  alias ViewOffice.Projects.Project
+  import Ecto.Query
   alias ViewOffice.Repo
+  alias ViewOffice.Projects.Project
+  alias ViewOffice.Accounts.Member
   alias ViewOffice.Scopes.User, as: UserScope
+  alias ViewOffice.Scopes.Member, as: MemberScope
 
-  @spec call(UserScope.t() | nil) :: [Project.t()] | [] | {:error, :unauthorized}
+  @spec call(UserScope.t() | MemberScope.t() | nil) :: [Project.t()] | [] | {:error, :unauthorized}
   def call(%UserScope{user: %{role: "ADMIN"}} = _scope) do
     Project
+    |> Repo.all()
+  end
+
+  def call(%UserScope{user: %{id: id}}) do
+    project_ids =
+      from(m in Member,
+        where: m.user_id == ^id,
+        select: m.project_id
+      )
+      |> Repo.all()
+
+    from(p in Project, where: p.id in ^project_ids)
     |> Repo.all()
   end
 
--- test/view_office/projects/services/all_test.exs
+++ test/view_office/projects/services/all_test.exs
@@ -1,25 +1,54 @@
 defmodule ViewOffice.Projects.Services.AllTest do
   use ViewOffice.DataCase
+
   alias ViewOffice.Projects.Services.All
   alias ViewOffice.Scopes.User, as: UserScope
 
   describe "Fetch all projects" do
     import ViewOffice.UserFixtures
     import ViewOffice.ProjectFixtures
+    import ViewOffice.MemberFixture
 
-    test "Should be able to return all projects if user are an admin" do
+    test "Should be able to return all projects if user is an admin" do
       user = user_fixture(%{role: "ADMIN", email: "banana@test.com"})
       scope = UserScope.for_user(user)
+
       project_fixture(%{name: "Project 1"})
+      project_fixture(%{name: "Project 2"})
+
       projects = All.call(scope)
-      assert length(projects) == 1
+      assert length(projects) == 2
     end
 
-    test "Should not be able to return all projects if user is not an admin" do
-      user = user_fixture(%{role: "COLLABORATOR", email: "banana@test.com"})
+    test "Should return only projects where the user is a member" do
+      user = user_fixture(%{role: "COLLABORATOR", email: "user1@test.com"})
       scope = UserScope.for_user(user)
+
+      project1 = project_fixture(%{name: "Project 1"})
+      project2 = project_fixture(%{name: "Project 2"})
+      project3 = project_fixture(%{name: "Project 3"})
+
+      # o usuário só é membro do projeto1 e projeto2
+      member_fixture(%{user_id: user.id, project_id: project1.id})
+      member_fixture(%{user_id: user.id, project_id: project2.id})
+
+      projects = All.call(scope)
+
+      assert Enum.map(projects, & &1.id) |> Enum.sort() ==
+               Enum.map([project1, project2], & &1.id) |> Enum.sort()
+
+      refute Enum.any?(projects, fn p -> p.id == project3.id end)
+    end
+
+    test "Should return empty list if user has no projects" do
+      user = user_fixture(%{role: "COLLABORATOR", email: "user2@test.com"})
+      scope = UserScope.for_user(user)
+
       project_fixture(%{name: "Project 1"})
-      assert {:error, :unauthorized} = All.call(scope)
+      project_fixture(%{name: "Project 2"})
+
+      projects = All.call(scope)
+      assert projects == []
     end
   end
 end
